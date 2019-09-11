e0e35c34068a4174f82ef15222edb9a0edde6021
Drew11 <drew.naumenko@gmail.com>
2019-09-11 19:40:37 +0300

    Updates

---
+++ precache-manifest.342681895578b4ca83528cac2f8d0a74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6458e57d922188daa19c97880bfb3f",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "9d34463ca1ffcf3441cc",
+    "url": "/test/static/css/main.35e57bbf.chunk.css"
+  },
+  {
+    "revision": "dc13cb30edeb559c7581",
+    "url": "/test/static/js/2.6fea29fc.chunk.js"
+  },
+  {
+    "revision": "9d34463ca1ffcf3441cc",
+    "url": "/test/static/js/main.35cc3f7b.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  }
+]);
